4ca0bbf21cd76a10535ce5af48b2d5617544ebad
Pedigree <pedigreesawatdeekrub@gmail.com>
2026-09-09 00:35:38 +0700

    Update apps/api/src/lib/storage.ts
    Co-authored-by: Phonlakrit Lookyee <167310929+taofuuu@users.noreply.github.com>

--- apps/api/src/lib/storage.ts
+++ apps/api/src/lib/storage.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { ApiError } from './ApiError';
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseKey = process.env.SUPABASE_SECRET_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
