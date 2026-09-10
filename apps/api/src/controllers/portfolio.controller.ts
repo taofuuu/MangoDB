92a62caf09c0aa38ca18ec22f91d929eb1d2c6f9
1999-Chin <chinsangwiset@gmail.com>
2026-09-10 13:24:32 +0700

    fix(portfolio): fix delete from storage by add import
    Co-authored-by: sirawit longjun <sirawit9874@gmail.com>

--- apps/api/src/controllers/portfolio.controller.ts
+++ apps/api/src/controllers/portfolio.controller.ts
@@ -6,7 +6,7 @@ import {
     portfolioSelect,
     toServicePortfolio,
 } from '../lib/portfolio';
-import { uploadToStorage, removeFromStorage } from '../lib/storage';
++import { uploadToStorage, removeFromStorage, BUCKET } from '../lib/storage';```
 import { omitUndefined } from '../lib/objects';
 import {
     isRecordNotFound,
